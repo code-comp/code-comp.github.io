989e1f649af48913fe2c8977fe4b36a0d13ba15b
Kyle Labelle <labelle.kn@gmail.com>
2022-10-18 21:56:18 -0400

    feat: profile page

---
+++ src/script.js
@@ -0,0 +1,11 @@
+const userInfo = {
+
+    "username":"klabelle",
+    "firstname":"Kyle",
+    "lastname":"Labelle",
+    "email":"klabe099@uottawa.ca"
+}
+
+const output = document.getElementById('output');
+
+output.innerHTML = "Username: " + userInfo.username + '<br>First Name: ' + userInfo.firstname + '<br>Last Name: ' + userInfo.lastname + '<br>Email Adress: ' + userInfo.email
