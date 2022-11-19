98fb05a015d45575047311b6cfeb80d26e8a4912
Kyle Labelle <113959933+kyle-labelle@users.noreply.github.com>
2022-11-19 16:42:14 -0500

    feat: update navbar (#38)
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>
    Co-authored-by: Daniel Thorp <arcanistzed@gmail.com>

---
+++ module/navbar.js
@@ -0,0 +1,28 @@
+import { getCookie } from "./utils.js";
+
+const image = document.querySelector("#profile");
+const id = getCookie("id");
+const token = getCookie("token");
+
+if (id) {
+	try {
+		const response = await (
+			await fetch(`https://code-comp.duckdns.org/api/users/${id}`, {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${token}`,
+				},
+			})
+        ).json();
+
+        if (!response.success) {
+            throw new Error(response.message);
+        }
+
+		const { user } = response;
+		image.src = user?.avatar ?? "";
+	} catch (error) {
+		console.error(error);
+	}
+}
